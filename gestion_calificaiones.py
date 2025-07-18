448bd6462346c892c708b613bd48da42df4c0172
Melqui Garcia <monterrosomelqui39@gmail.com>
2025-07-18 09:19:11 -0600

    def, menu de estudiantes

--- gestion_calificaiones.py
+++ gestion_calificaiones.py
@@ -4,4 +4,4 @@ def agregar_estudiante():
  nombre = input("ingrese el nombre del estudiante:").strip()
  if nombre in registro_estudiante:
    print(f"(el estudiante '{nombre}' ya esta registrado con calificaciones {registro_estudiante}")
-   return
+   retur
